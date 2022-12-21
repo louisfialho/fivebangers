52db3e894fe044e5dbe1ea8d75e8174bfde19c81
Louis Fialho <louis.fialho@hec.edu>
2022-12-21 21:39:42 +0000

    create banger

---
+++ app/controllers/tracks_controller.rb
@@ -0,0 +1,31 @@
+class TracksController < ApplicationController
+
+  def create
+    # create track
+    @track = Track.new(track_params)
+    @track.save
+    # get position
+    @position = params[:position]
+    # get user
+    @user = current_user
+    # check if user has a track with this position
+    if @user.user_track_relationships.where(position: @position).empty? == false
+      relationship = @user.user_track_relationships.where(position: @position).first
+      relationship.position = "archived"
+      relationship.save
+    end
+    # assign the new track to the user and specify position
+    UserTrackRelationship.create(user: @user, track: @track, position: @position)
+    # refresh page
+    respond_to do |format|
+      format.html { redirect_to user_path(current_user) }
+    end
+  end
+
+  private
+
+  def track_params
+    params.require(:track).permit(:youtube_url, :position)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'users/:id', to: 'users#show', as: :user
+  get "tracks/new", to: "tracks#new"
+  post "tracks", to: "tracks#create", as: :create_track
+
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
