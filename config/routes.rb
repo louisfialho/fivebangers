d66d56a948ae5bf633bc43de3b808e655a581008
Louis Fialho <louis.fialho@hec.edu>
2022-12-22 11:57:07 +0000

    lp skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   get 'users/:id', to: 'users#show', as: :user
   get "tracks/new", to: "tracks#new"
   post "tracks", to: "tracks#create", as: :create_track
-
-
-
+  get '/errors/club_is_full', to: 'pages#club_is_full', as: "club_is_full"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
