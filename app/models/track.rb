3e349705ee403a96cfdb1b52debbefc3fa8f056a
Louis Fialho <louis.fialho@hec.edu>
2022-12-22 09:37:35 +0000

    k

--- app/models/track.rb
+++ app/models/track.rb
@@ -3,5 +3,5 @@ class Track < ApplicationRecord
   has_many :users, through: :user_track_relationships
 
   validates :youtube_url, presence: true #must have a url
-  # validates :youtube_url, format: { with: /\A.*@.*\.com\z/ } #regex a tester pr youtube link
+  #validates :youtube_url, format: { with: /^http:\/\/www\.youtube\.com\/watch\?v=([a-zA-Z0-9_-]*)/ } #regex a tester pr youtube link
 end
