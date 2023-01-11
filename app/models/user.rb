d1caea7dbc33c814d56fd36bc8cb91d211d7ba3a
Louis Fialho <louis.fialho@hec.edu>
2023-01-11 22:28:37 +0000

    k

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  before_create do
+    catch :missingyear do  #:missingyear acts as a label
+      for a in ("a".."z")
+        for b in ("a".."z")
+          for c in (0..9)
+            for d in (0..9)
+              for e in (0..9)
+                proposed_username = "#{a}#{b}#{c}#{d}#{e}".upcase()
+                if User.find_by(username: proposed_username).nil?
+                  self.username = proposed_username
+                  throw :missingyear
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
 end
--- test.rb
+++ test.rb
@@ -1,6 +1,13 @@
 require File.expand_path('config/environment', __dir__)
-require 'securerandom'
 
-youtube_url = "https://www.youtube.com/watch?v=B-d5l0nkStA"
-youtube_url = youtube_url + SecureRandom.urlsafe_base64 if youtube_url.length < 50
-p youtube_url
+for a in ("a".."z")
+  for b in ("a".."z")
+    for c in (0..9)
+      for d in (0..9)
+        for e in (0..9)
+          p "#{a}#{b}#{c}#{d}#{e}".upcase()
+        end
+      end
+    end
+  end
+end
