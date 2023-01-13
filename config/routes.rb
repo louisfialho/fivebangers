9e297142f34ccf218bc2c014b6b3589e7dc9eea7
Louis Fialho <louis.fialho@hec.edu>
2023-01-13 20:08:59 +0000

    added description

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'pages/welcome', to: 'pages#welcome', as: "welcome"
   get 'users/:id', to: 'users#show', as: :user
   get "users/:id/edit", to: "users#edit", as: :edit_user
+  patch "users/:id", to: "users#update"
   get "tracks/new", to: "tracks#new"
   post "tracks", to: "tracks#create", as: :create_track
   get '/errors/club_is_full', to: 'pages#club_is_full', as: "club_is_full"
