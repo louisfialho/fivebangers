73ec59ba184bbd44b197300b239eac4f3c7f5550
Louis Fialho <louis.fialho@hec.edu>
2023-01-14 15:13:44 +0000

    should now be possible to edit 1st banger on mobile

--- app/javascript/components/editProfile.js
+++ app/javascript/components/editProfile.js
@@ -1,26 +1,13 @@
 const editProfile = () => {
 
-  const link1 = document.getElementById('link-1');
-  const cursor1 = document.getElementById('cursor-1');
-  const formTrack1 = document.getElementById('form-track-1');
   const inputFormTrack1 = document.getElementById('input-form-1');
+  const formTrack1 = document.getElementById('form-track-1');
 
-  link1.addEventListener('click', function(event) {
-    cursor1.style.display = "block";
-    link1.style.display = "none";
+  // submit form
 
-    document.addEventListener("paste", (event) => {
-      var clipboardData, pastedData;
-      event.stopPropagation();
-      event.preventDefault();
-      clipboardData = event.clipboardData || window.clipboardData;
-      pastedData = clipboardData.getData('Text');
-      inputFormTrack1.value = pastedData;
-      formTrack1.submit()
-      // reload page
-    });
+  inputFormTrack1.addEventListener("paste", (event) => {
+    formTrack1.submit()
   });
-
 }
 
 export { editProfile };
