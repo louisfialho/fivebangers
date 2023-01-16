05e823f506034a778da75dd7cbbde91e8aaf3b4a
Louis Fialho <louis.fialho@hec.edu>
2023-01-16 19:31:34 +0000

    added flashing edit

--- app/javascript/components/blinking.js
+++ app/javascript/components/blinking.js
@@ -1,19 +1,33 @@
 const blinking = () => {
 
+  // all of the above could be refactored easily
+
   const descriptionInput = document.getElementById('edit-description')
+  const tracks = document.querySelectorAll(".track-forms");
 
   descriptionInput.addEventListener('focus', (event) => {
     descriptionInput.style.animation = "blinker 1.5s linear";
     descriptionInput.style['-webkit-animation'] = "blinker 1.5s linear";
-    console.log("hi");
   });
 
   descriptionInput.addEventListener('focusout', (event) => {
     descriptionInput.style.animation = "blinker 1.5s linear infinite";
     descriptionInput.style['-webkit-animation'] = "blinker 1.5s linear infinite";
-    console.log("ho");
   });
 
+  tracks.forEach(function(elem) {
+    elem.addEventListener("focus", (event) => {
+      elem.style.animation = "blinker 1.5s linear";
+      elem.style['-webkit-animation'] = "blinker 1.5s linear";
+    });
+  });
+
+  tracks.forEach(function(elem) {
+    elem.addEventListener("focusout", (event) => {
+      elem.style.animation = "blinker 1.5s linear infinite";
+      elem.style['-webkit-animation'] = "blinker 1.5s linear infinite";
+    });
+  });
 
 }
 
