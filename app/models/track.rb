dba95d6c2083251cce53c764577a1ba4eafd515f
Louis Fialho <louis.fialho@hec.edu>
2023-01-19 19:19:16 +0000

    k

--- app/models/track.rb
+++ app/models/track.rb
@@ -5,7 +5,5 @@ class Track < ApplicationRecord
   has_many :users, through: :user_track_relationships
 
   validates :youtube_url, presence: true #must have a url
-  validates :youtube_url, format: { with: /\A((http|https):\/\/)?(www.)?youtube.com\//,
-message: "use a YouTube url" }
 
 end
