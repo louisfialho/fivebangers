0e5cc7bb6897741d54e260b881dc1284db8c4591
Louis Fialho <louis.fialho@hec.edu>
2023-01-20 20:06:03 +0000

    urls should be responsive

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
 module ApplicationHelper
-  def device
-    agent = request.user_agent
-    return "tablet" if agent =~ /(tablet|ipad)|(android(?!.*mobile))/i
-    return "mobile" if agent =~ /Mobile/
-    return "desktop"
-  end
 end
