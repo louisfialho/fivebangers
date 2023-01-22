63e18035e1caf6a92362a07338e98dc48a6f68f6
Louis Fialho <louis.fialho@hec.edu>
2023-01-22 16:31:12 +0000

    k

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  has_many :user_track_relationships
+  has_many :user_track_relationships, dependent: :delete_all
   has_many :tracks, through: :user_track_relationships
 
   # Include default devise modules. Others available are:
