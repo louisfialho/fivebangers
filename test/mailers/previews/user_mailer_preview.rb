b0fe94cc3da6f0cf78f52f8672b49103dc8ad3df
Louis Fialho <louis.fialho@hec.edu>
2023-03-19 19:51:27 +0000

    k

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
+  def welcome_email
+    user = User.last
+    UserMailer.with(user: user).welcome_email
+  end
+
 end
